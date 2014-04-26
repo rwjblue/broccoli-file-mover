4dcba05231b47f2312422b5ec452418736842aa9
Robert Jackson <robert.w.jackson@me.com>
2014-04-26 13:11:05 -0400

    Update to use broccoli-caching-writer.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var path = require('path');
-var Writer = require('broccoli-writer');
+var CachingWriter = require('broccoli-caching-writer');
 var helpers = require('broccoli-kitchen-sink-helpers')
 
-Mover.prototype = Object.create(Writer.prototype);
+Mover.prototype = Object.create(CachingWriter.prototype);
 Mover.prototype.constructor = Mover;
 function Mover (inputTree, options) {
   if (!(this instanceof Mover)) return new Mover(inputTree, options);
@@ -23,26 +23,24 @@ Mover.prototype._copyFile = function (directory, source, destination, leaveOrigi
   if (!leaveOriginal) { fs.unlinkSync(path.join(directory, source)); }
 };
 
-Mover.prototype.write = function (readTree, destDir) {
-  var self = this
+Mover.prototype.updateCache = function (srcDir, destDir) {
+  var self = this;
 
-  return readTree(this.inputTree).then(function (srcDir) {
-    helpers.copyRecursivelySync(srcDir, destDir);
+  helpers.copyRecursivelySync(srcDir, destDir);
 
-    if (Array.isArray(self.files)) {
-      self.files.forEach(function(file) {
-        self._copyFile(destDir, file.srcFile, file.destFile, file.copy);
-      });
-    } else if (self.files) {
-      for (var key in self.files) {
-        if (self.files.hasOwnProperty(key)) {
-          self._copyFile(destDir, key, self.files[key], self.copy);
-        }
+  if (Array.isArray(self.files)) {
+    self.files.forEach(function(file) {
+      self._copyFile(destDir, file.srcFile, file.destFile, file.copy);
+    });
+  } else if (self.files) {
+    for (var key in self.files) {
+      if (self.files.hasOwnProperty(key)) {
+        self._copyFile(destDir, key, self.files[key], self.copy);
       }
-    } else {
-      self._copyFile(destDir, self.srcFile, self.destFile, self.copy);
     }
-  })
+  } else {
+    self._copyFile(destDir, self.srcFile, self.destFile, self.copy);
+  }
 };
 
 module.exports = Mover;
